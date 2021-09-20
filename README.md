cc79e64129d50d32db8d6e385971ea44b0f496be
eia485 <eia485git@gmail.com>
2021-09-20 10:00:16 -0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,7 @@
+# YouWantDarkmode?
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that swaps how black and white are defined. this makes everything dark mode and causes some issues. mainly: your dash will be black and colors will be desynced.
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [YouWantDarkmode.dll](https://github.com/eia485/NeosYouWantDarkmode/releases/latest/download/YouWantDarkmode.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
---
+++ YouWantDarkmode/YouWantDarkmode.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using BaseX;
+
+namespace YouWantDarkmode
+{
+	public class YouWantDarkmode : NeosMod
+	{
+		public override string Name => "YouWantDarkmode?";
+		public override string Author => "eia485";
+		public override string Version => "1.0.0";
+		public override string Link => "https://github.com/EIA485/NeosYouWantDarkmode/";
+		public override void OnEngineInit()
+		{
+			Harmony harmony = new Harmony("net.eia485.YouWantDarkmode?");
+			harmony.PatchAll();
+		}
+
+		[HarmonyPatch(typeof(color), MethodType.Getter)]
+		class MoreReferenceProxiesPatch
+		{
+			[HarmonyPrefix]
+			[HarmonyPatch("Black")]
+			static bool BlackPreFix(ref color __result)
+			{
+				__result = new color(1);
+				return false;
+			}
+
+			[HarmonyPrefix]
+			[HarmonyPatch("White")]
+			static bool whitePreFix(ref color __result)
+			{
+				__result = new color(0);
+				return false;
+			}
+		}
+	}
+}
